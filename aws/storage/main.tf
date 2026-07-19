15097ce1550c3659fd1f62e59d6466d6cfd74511
anishakurulkar-org <anish.a.kurulkar@avyka.com>
2026-07-19 21:17:51 +0530

    Update main.tf

--- aws/storage/main.tf
+++ aws/storage/main.tf
@@ -17,15 +17,3 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 
 }
-
-resource "aws_s3_bucket_versioning" "versioning" {
-
-  bucket = aws_s3_bucket.storage.id
-
-  versioning_configuration {
-
-    status = var.enable_versioning ? "Enabled" : "Suspended"
-
-  }
-
-}
